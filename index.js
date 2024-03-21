64deac8683f1e2fbbca29eff764e647af615205e
mohNourBoshi <muhammadnour963963@gmail.com>
2024-03-22 00:48:01 +0300

    add path num

--- index.js
+++ index.js
@@ -16,6 +16,36 @@ app.post('/sms', express.json(),(req, res) => {
   res.send(i);
 });
 
+
+
+// Route to handle POST requests
+app.get('/num', (req, res) => {
+    // Read the number from the file
+    fs.readFile('number.txt', 'utf8', (err, data) => {
+      if (err) {
+        console.error('Error reading file:', err);
+        return res.status(500).json({ error: 'Internal Server Error' });
+      }
+  
+      // Parse the content of the file as a number
+      let number = parseInt(data);
+  
+      // Increment the number by 1
+      number++;
+  
+      // Write the incremented number back to the file
+      fs.writeFile('number.txt', number.toString(), 'utf8', err => {
+        if (err) {
+          console.error('Error writing file:', err);
+          return res.status(500).json({ error: 'Internal Server Error' });
+        }
+  
+        // Send the incremented number as a response
+        res.json({ incrementedNumber: number });
+      });
+    });
+  });
+  
 // Define the port number
 const port = 3000;
 
