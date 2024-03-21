fbeea950fddbd35e545b003c311b6fc1d50855b3
mohNourBoshi <muhammadnour963963@gmail.com>
2024-03-21 22:43:52 +0300

    say hi

---
+++ index.js
@@ -0,0 +1,18 @@
+// Import required modules
+const express = require('express');
+
+// Create an instance of express
+const app = express();
+
+// Define a route that responds with "Hi" when accessed
+app.get('/', (req, res) => {
+  res.send('Hi');
+});
+
+// Define the port number
+const port = 3000;
+
+// Start the server and listen on the defined port
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
